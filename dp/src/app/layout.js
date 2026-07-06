64326a2f7745a137024dae534804bb8db744ca03
alimohammad <alimohammadsa98@gmail.com>
2026-07-06 14:19:14 +0530

    Fixed casing for the sessionwrapper folder

--- dp/src/app/layout.js
+++ dp/src/app/layout.js
@@ -2,7 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footers";
-import SessionWrapper from "./components/sessionwrapperr";
+import SessionWrapper from "./components/sessionwrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",

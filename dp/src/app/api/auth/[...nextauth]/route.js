8af0e1a70698a0e690a1999f30ae9c102d35e7b6
alimohammad <alimohammadsa98@gmail.com>
2026-07-19 22:49:48 +0530

    push it

--- dp/src/app/api/auth/[...nextauth]/route.js
+++ dp/src/app/api/auth/[...nextauth]/route.js
@@ -1,38 +1,4 @@
-  // import NextAuth from "next-auth";
-  // import GithubProvider from "next-auth/providers/github";
-  // import connectdb from "@/db/connectdb";
-  // import User from "../../../models/User";
 
-
-  // const handler = NextAuth({
-  //   providers: [
-  //     GithubProvider({
-  //       clientId: process.env.GITHUB_ID,
-  //       clientSecret: process.env.GITHUB_SECRET,
-  //     }),
-  //   ],
-
-  //   callbacks: {
-  //     async signIn({ user }) {
-  //       await connectdb();
-
-  //       const existingUser = await User.findOne({
-  //         email: user.email,
-  //       });
-
-  //       if (!existingUser) {
-  //         await User.create({
-  //           name: user.name,
-  //           email: user.email,
-  //         });
-  //       }
-
-  //       return true;
-  //     },
-  //   },
-  // });
-
-  // export { handler as GET, handler as POST };
 
 
   import NextAuth from "next-auth";
